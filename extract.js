bba5882d6735557e9051df1bcd0b27766ee9e7f3
frankingardia <fingardia@gmail.com>
2016-12-08 22:14:32 +1100

    -Task 2 Part 1: Output seems to now be correct and matching the output that is expected in the test, next step is to set up the code in the test environment

--- extract.js
+++ extract.js
@@ -2,8 +2,7 @@ module.exports.extract = function(window) {
   
   // Write your solution to Task #2 - Extract Metadata task here
 
-    var inputs = window.document.getElementsByTagName("tr");
-    console.log(inputs);
+
 }
 
 var jsdom = require("jsdom");
@@ -14,28 +13,32 @@ jsdom.env(
     ["~/WebstormProjects/fillr-test-a/extract.js"],
     function (err, window) {
 
-        var inputs = window.document.getElementsByTagName("tr");
-        var output = {};
-        var addString = "";
+        var inputs = window.document.getElementsByTagName("tr"); //pulls in everything needed
+        var output = {}; //the final object to be returned
+        var addString = ""; //model for metadata string
 
         for (var a = 0; a < inputs.length; a++)
         {
+            //checks whether we have an input or a select object and works accordingly
             if (inputs[a].getElementsByTagName("input")[0] != undefined)
             {
-                addString = inputs[a].textContent + inputs[a].getElementsByTagName("input")[0].name;
-                console.log(addString);
+                addString = inputs[a].textContent.replace(/\s\s+/g, '') + " " + inputs[a].getElementsByTagName("input")[0].name.replace(/\s\s+/g, '');
+
+                //output.push({key: inputs[a].textContent.replace(/\s/g, ''), value: addString});
+                output[inputs[a].textContent.replace(/\s\s+/g, '')] = addString;
 
 
             }
             else if (inputs[a].getElementsByTagName("select")[0] != undefined) {
 
-                addString = inputs[a].getElementsByTagName("td")[0].textContent + inputs[a].getElementsByTagName("select")[0].name;
-                console.log(addString);
+                addString = inputs[a].getElementsByTagName("td")[0].textContent.replace(/\s\s+/g, '') + " " + inputs[a].getElementsByTagName("select")[0].name.replace(/\s\s+/g, '');
+                //output.push({key: inputs[a].getElementsByTagName("td")[0].textContent.replace(/\s/g, ''), value: addString});
+                output[inputs[a].getElementsByTagName("td")[0].textContent.replace(/\s\s+/g, '')] = addString;
             }
 
         }
 
-
+        console.log(JSON.stringify(output));
 
     }
 );
