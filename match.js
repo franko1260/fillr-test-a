fd2ebe1c79b6fc65a1d154c969bac78cf8db8cc9
frankingardia <fingardia@gmail.com>
2016-12-09 16:28:48 +1100

    -Task 2 Part 1: Complete! -Task 2 Part 2: Very simple, just a matter of extracting the values we wanted with a simple for loop and if statement. If I had to make more commits for it I would've had to commit after each line written! Passed test.

--- match.js
+++ match.js
@@ -2,15 +2,20 @@ module.exports.match = function(hash) {
 
   // Write your solution to Task #2 - Match Metadata task here
 
-  
+    var keys = Object.keys(hash); //copy keys to new variable for easy referencing in the for loop
+    var output = [];
+
+    var searchString = "Ecom_Payment_Card_ExpDate"; //This string is common with the three fields that the test expects to see returned
+
+    for (var a = 0; a < Object.keys(hash).length; a++)
+    {
+        var metaString = hash[keys[a]]; //entry value we're currently working with
+
+        if (metaString.indexOf(searchString) !== -1)
+        {
+            output.push(keys[a]); //return the key of the corresponding value we're working with
+        }
+    }
+
+    return output;
 }
-
-/*jsdom.env(
-    "http://autofill.mozdev.org/autofilltest.html",
-    ["~/WebstormProjects/fillr-test-a/match.js"],
-    function (err, window) {
-        //dhohfad
-
-
-
-    }*/
