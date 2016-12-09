ebc064182b5f4fec977eecea6444f2d0d8cc9f14
frankingardia <fingardia@gmail.com>
2016-12-09 15:25:35 +1100

    -Task 2 Part 1: Tests with Mocha and Chai set up, after minor modifications the code passes the first test!

--- extract.js
+++ extract.js
@@ -2,43 +2,34 @@ module.exports.extract = function(window) {
   
   // Write your solution to Task #2 - Extract Metadata task here
 
+    var inputs = window.document.getElementsByTagName("tr"); //pulls in everything needed
+    var output = {}; //the final object to be returned
+    var addString = ""; //model for metadata string
 
-}
-
-var jsdom = require("jsdom");
-const fs = require("fs");
-
-jsdom.env(
-    "http://autofill.mozdev.org/autofilltest.html",
-    ["~/WebstormProjects/fillr-test-a/extract.js"],
-    function (err, window) {
-
-        var inputs = window.document.getElementsByTagName("tr"); //pulls in everything needed
-        var output = {}; //the final object to be returned
-        var addString = ""; //model for metadata string
-
-        for (var a = 0; a < inputs.length; a++)
+    for (var a = 0; a < inputs.length; a++)
+    {
+        //checks whether we have an input or a select object and works accordingly
+        if (inputs[a].getElementsByTagName("input")[0] != undefined)
         {
-            //checks whether we have an input or a select object and works accordingly
-            if (inputs[a].getElementsByTagName("input")[0] != undefined)
-            {
-                addString = inputs[a].textContent.replace(/\s\s+/g, '') + " " + inputs[a].getElementsByTagName("input")[0].name.replace(/\s\s+/g, '');
+            addString = inputs[a].textContent.replace(/\s\s+/g, '').trim() + " "
+                + inputs[a].getElementsByTagName("input")[0].name.replace(/\s\s+/g, '');
 
-                //output.push({key: inputs[a].textContent.replace(/\s/g, ''), value: addString});
-                output[inputs[a].textContent.replace(/\s\s+/g, '')] = addString;
+            //output.push({key: inputs[a].textContent.replace(/\s/g, ''), value: addString});
+            output[inputs[a].textContent.replace(/\s\s+/g, '').trim()] = addString;
 
-
-            }
-            else if (inputs[a].getElementsByTagName("select")[0] != undefined) {
-
-                addString = inputs[a].getElementsByTagName("td")[0].textContent.replace(/\s\s+/g, '') + " " + inputs[a].getElementsByTagName("select")[0].name.replace(/\s\s+/g, '');
-                //output.push({key: inputs[a].getElementsByTagName("td")[0].textContent.replace(/\s/g, ''), value: addString});
-                output[inputs[a].getElementsByTagName("td")[0].textContent.replace(/\s\s+/g, '')] = addString;
-            }
 
         }
+        else if (inputs[a].getElementsByTagName("select")[0] != undefined) {
 
-        console.log(JSON.stringify(output));
+            addString = inputs[a].getElementsByTagName("td")[0].textContent.replace(/\s\s+/g, '') + " "
+                + inputs[a].getElementsByTagName("select")[0].name.replace(/\s\s+/g, '');
+
+            //output.push({key: inputs[a].getElementsByTagName("td")[0].textContent.replace(/\s/g, ''), value: addString});
+            output[inputs[a].getElementsByTagName("td")[0].textContent.replace(/\s\s+/g, '')] = addString;
+        }
 
     }
-);
+
+    return output;
+
+}
--- match.js
+++ match.js
@@ -1,5 +1,16 @@
 module.exports.match = function(hash) {
 
   // Write your solution to Task #2 - Match Metadata task here
+
   
 }
+
+/*jsdom.env(
+    "http://autofill.mozdev.org/autofilltest.html",
+    ["~/WebstormProjects/fillr-test-a/match.js"],
+    function (err, window) {
+        //dhohfad
+
+
+
+    }*/
